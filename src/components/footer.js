eb5c254401cb42031b29ee6af0b8c26520f65c3a
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2020-12-18 19:35:40 +0100

    feat: setup bulma css framework

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,16 +12,16 @@ const Footer = () => {
         <div class = "footer">
             <div class="footer-icons">
                 <ListLink to="https://web.facebook.com/pinafricadotorg" class="footer-social_media">
-                    <img src="./facebook.svg" alt="Facebook Pinafrica" />
+                    <img src="./static/facebook.svg" alt="Facebook Pinafrica" />
                 </ListLink>
                 <ListLink to="https://twitter.com/pinafricadotorg" class="footer-social_media">
-                    <img src="./twitter.svg" alt="Twitter Pinafrica" />
+                    <img src="./static/twitter.svg" alt="Twitter Pinafrica" />
                 </ListLink>
                 <ListLink to="https://www.linkedin.com/company/pinafricadotorg" class="footer-social_media">
-                    <img src="./linkedin.svg" alt="Linkedin Pinafrica" />
+                    <img src="./static/linkedin.svg" alt="Linkedin Pinafrica" />
                 </ListLink>
                 <ListLink to="https://www.instagram.com/pinafricadotorg/" class="footer-social_media">
-                    <img src="./instagram.svg" alt="Instagram Pinafrica" />
+                    <img src="./static/instagram.svg" alt="Instagram Pinafrica" />
                 </ListLink>
             </div>
             <span> &copy; PINAFRICA 2020</span>
