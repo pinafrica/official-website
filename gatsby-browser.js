e08a5ad536c05c7053a734a0225372d19263ce2f
Chiamaka Osumgba <osumgbachiamaka@gmail.com>
2020-12-19 18:52:03 +0100

    Revert "feat: setup bulma css framework"
    This reverts commit 7e373856c65bbcf4bb9373c6e284d5c6c7b0dc21.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
-// import "./node_modules/bulma/bulma.sass"
-import "./src/assets/styles/css/global.css"
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+import "./src/assets/styles/global.css"
 
